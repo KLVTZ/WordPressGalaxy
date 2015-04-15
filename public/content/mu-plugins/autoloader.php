b5e309941097286832102574ebd098cf90fccbd9
Justin Page <xjustinpagex@gmail.com>
2015-04-14 18:35:14 -0700

    update: SPL State autoload

---
+++ public/content/mu-plugins/autoloader.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Plugin Name: App Classes
+ * Description: Register the autoloader for all our app classes all of which
+ * must use the WPPlugins top level
+ * Author: Justin Page
+ * Provides: Classes
+ *
+ */
+
+spl_autoload_register(function ($class)
+{
+	$segments = array_filter(explode("\\", $class));
+	$first = array_shift($segments);
+
+	if ($first === "WPPlugins") {
+		$path = dirname(__DIR__) . "/classes/" . "/" . implode("/", $segments) . ".php";
+
+	} else {
+		array_unshift($segments, $first);
+		$path = WP_PLUGINS_DIR	. "/" . implode("/", $segments) . ".php";
+	}
+	if (file_exists($path))
+		include $path;
+});
